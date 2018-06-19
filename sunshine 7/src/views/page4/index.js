b3efe66469bfbb9858dfe1a56ed3cf528266dd3f
ccx <2367967515@qq.com>
2018-06-19 11:10:17 +0800

    page4修改

--- sunshine 7/src/views/page4/index.js
+++ sunshine 7/src/views/page4/index.js
@@ -5,10 +5,7 @@ import axios from 'axios';
 import './index.less';
 import Ranktop from './images/rank_top.png';
 import Rankbtm from './images/rank_btm.png';
-import blue_b_ico1 from './images/blue_b_ico1.png';
-import blue_b_ico2 from './images/blue_b_ico2.png';
-import blue_b_ico3 from './images/blue_b_ico3.png';
-import blue_b_ico4 from './images/blue_b_ico4.png';
+import tpl from './tpl';
 
 export default () => {
   // $('.check-box').on('click', 'ul li input', function() {
@@ -20,106 +17,7 @@ export default () => {
   //     .children('div')
   //     .removeClass('checked');
   // });
-  document.getElementById('page4').innerHTML = `    <div class="content">
-<div class="box">
-  <div class="titlebox">
-    <h3>历史累计数据统计</h3>
-  </div>
-  <div class="code">
-    <ul class="code1">
-      <li>
-        <img src="${blue_b_ico1}" alt="">
-      </li>
-      <li>
-        <span>运单总数</span>
-        <span>
-          <i id="billCount">20000</i> 个</span>
-      </li>
-    </ul>
-    <ul class="code2">
-      <li>
-        <img src="${blue_b_ico2}" alt="">
-      </li>
-      <li>
-        <span>本地车辆</span>
-        <span>
-          <i id="localCount">20000</i> 个</span>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <img src="${blue_b_ico3}" alt="">
-      </li>
-      <li>
-        <span>外地车辆</span>
-        <span>
-          <i id="foreignCount">20000</i> 个</span>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <img src="${blue_b_ico4}" alt="">
-      </li>
-      <li>
-        <span>总运量</span>
-        <span>
-          <i id="weight">20000</i> 吨</span>
-      </li>
-    </ul>
-  </div>
-  <div class="box">
-    <div class="box1">
-      <div class="chartsBg">
-        <ul class="title">
-          <li>运输量
-            <i id="loadingCount">20000</i>
-            <em>吨</em>
-          </li>
-        </ul>
-        <ul class="title title2">
-          <li>卸货量
-            <i id="unloadingCount">20000</i>
-            <em>吨</em>
-          </li>
-        </ul>
-        <div class="check-box">
-          <ul>
-            <li>
-              <input type="radio" id="radio6">
-              <div class="checked"></div>
-              <label for="radio6">企业</label>
-            </li>
-            <li>
-              <input type="radio" id="radio7">
-              <div></div>
-              <label for="radio7">货物</label>
-            </li>
-
-
-          </ul>
-        </div>
-      </div>
-      <div class="chartsbox">
-        <!-- <div class="ranking">
-          <ul>
-            <li>01</li>
-            <li>02</li>
-            <li>03</li>
-            <li>04</li>
-            <li>05</li>
-            <li>06</li>
-            <li>07</li>
-            <li>08</li>
-            <li>09</li>
-            <li>10</li>
-          </ul>
-        </div> -->
-        <div class="main9"></div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>`;
+  document.getElementById('page4').innerHTML = tpl;
   var mycharts1 = echarts.init(document.querySelector('.main9'));
 
   const getOption = ({
